54731af2ec0603ba29cdc5cf47128d52780259bd
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-09-09 15:10:21 +0300

    3 commit

--- src/probagit2/ProbaGit2.java
+++ src/probagit2/ProbaGit2.java
@@ -8,6 +8,7 @@ public class ProbaGit2 {
     public static void main(String[] args) {
     System.out.println("First version of program");
     System.out.println("Second version of program");
+    System.out.println("Third version of program");
     }
     
 }
